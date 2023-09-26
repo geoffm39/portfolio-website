14b5af2e5a02e8b8c80dd7e2d5cd65162604553c
Geoffrey Martin <geoffm39@gmail.com>
2023-09-26 21:11:15 +1000

    update templates

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,17 @@
 from flask import render_template, redirect, url_for
 from app import app
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
