6126283e76ee634d0b3c6669e4d56ca46c722774
Geoffrey Martin <geoffm39@gmail.com>
2023-10-02 12:24:08 +1100

    setup contact page with email

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,20 @@
-from flask import render_template, redirect, url_for
+from flask import render_template, request
 from app import app
+import os
+import smtplib
+
+MY_EMAIL = os.getenv('MY_EMAIL')
+APP_EMAIL = os.getenv('APP_EMAIL')
+EMAIL_PASS = os.getenv('EMAIL_PASS')
+
+
+def send_email(name, email, subject, message):
+    with smtplib.SMTP('smtp.gmail.com', port=587) as connection:
+        connection.starttls()
+        connection.login(APP_EMAIL, EMAIL_PASS)
+        connection.sendmail(from_addr=APP_EMAIL,
+                            to_addrs=MY_EMAIL,
+                            msg=f"Subject:Portfolio Message\n\nName: {name}\nEmail: {email}\nSubject: {subject}\n{message}")
 
 
 @app.route('/')
@@ -7,8 +22,12 @@ def home():
     return render_template('index.html', homepage=True)
 
 
-@app.route('/contact')
+@app.route('/contact', methods=['GET', 'POST'])
 def contact():
+    if request.method == 'POST':
+        data = request.form
+        send_email(data['name'], data['email'], data['subject'], data['message'])
+        return render_template('contact.html', contact_page=True, sent_message=True)
     return render_template('contact.html', contact_page=True)
 
 
